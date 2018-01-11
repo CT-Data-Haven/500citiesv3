84be820cad68485a9975256fc4c599284269929c
camille-s <camille.seaberry@gmail.com>
2018-01-11 11:17:10 -0500

    Add updates to date, intro

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
 		<div className="Footer">
 			<Segment basic secondary>
 				<p>
-					<strong>Source: </strong>Centers for Disease Control and Prevention. (2016). <a href="https://www.cdc.gov/500cities/">500 Cities Project.</a> Census tracts shown are the 2015 US Census Bureau <a href="https://www.census.gov/geo/maps-data/data/tiger.html">TIGER boundaries</a>.
+					<strong>Source: </strong>Centers for Disease Control and Prevention. (2017). <a href="https://www.cdc.gov/500cities/">500 Cities Project.</a> Census tracts shown are the 2015 US Census Bureau <a href="https://www.census.gov/geo/maps-data/data/tiger.html">TIGER boundaries</a>.
 				</p>
 			</Segment>
 		</div>
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -9,10 +9,13 @@ const Intro = () => {
 					The following data is from the 500 Cities Project of the CDC, CDC Foundation, and Robert Wood Johnson Foundation. The project makes available estimates of a variety of measures of public health at the census tract-level, allowing cities to better understand health issues locally.
 				</p>
 				<p>
-					The map here displays city- and tract-level data for the 8 cities in Connecticut included in the project. More information on the 500 Cities Project is <a href="https://www.cdc.gov/500cities/">available from the CDC</a>.
+					The map here displays city- and tract-level data for the 8 cities in Connecticut included in the project. More information on the 500 Cities Project is <a href="https://www.cdc.gov/500cities/">available from the CDC</a>. Data was updated by CDC in December 2017.
 				</p>
 				<p>
 					Values for cities are shown in the chart at bottom. Choose a city to view its data by tract. Choose the tract summary view to see a histogram of all tract-level data.
+				</p>
+				<p>
+					See how DataHaven used this data for an in-depth study on <a href="http://ctdatahaven.org/reports/merging-500-cities-and-connecticut-data-health-equity">health equity in Connecticut cities</a>.
 				</p>
 			</Message>
 		</div>
